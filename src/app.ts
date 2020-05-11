8673c44fee1d77485776ee36ee2936536e11fb6a
risavkarna <risavkarna@gmail.com>
2020-05-11 11:29:33 +0545

    add global error handling middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import bodyParser from "body-parser";
 import { RegisterRoutes } from "../build/routes";
 
-import express, { Response as ExResponse, Request as ExRequest } from "express";
+import express, { Response as ExResponse, Request as ExRequest, NextFunction } from "express";
 
 import swaggerUi from "swagger-ui-express";
+import { ValidateError } from "tsoa";
 
 export const app = express();
 
@@ -15,6 +16,7 @@ app.use(
 );
 app.use(bodyParser.json());
 
+// swagger API endpoint
 app.use("/docs", swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
   return res.send(
     swaggerUi.generateHTML(await import('../build/swagger.json'))
@@ -22,3 +24,33 @@ app.use("/docs", swaggerUi.serve, async (_req: ExRequest, res: ExResponse) => {
 });
 
 RegisterRoutes(app);
+
+// global error handling middleware
+app.use(function errorHandler(
+  err: unknown,
+  req: ExRequest,
+  res: ExResponse,
+  next: NextFunction
+): ExResponse | void {
+  if (err instanceof ValidateError) {
+    console.warn(`Caught Validation Error for ${req.path}:`, err.fields);
+    return res.status(422).json({
+      message: "Validation Failed",
+      details: err?.fields,
+    });
+  }
+  if (err instanceof Error) {
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+
+  next();
+});
+
+//for all unregistered routes
+app.use(function notFoundHandler(_req, res: ExResponse) {
+  res.status(404).send({
+    message: "Not Found",
+  });
+});
