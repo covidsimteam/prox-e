0969f3ce1b95b510606837ce3c2fdcfb9966b705
Risav Karna <risavkarna@gmail.com>
2020-08-15 18:42:10 +0545

    (minor) add Nb to LogoutComponent

--- hi/dashboard/src/app/auth/logout/logout.component.ts
+++ hi/dashboard/src/app/auth/logout/logout.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NbLogoutComponent } from '@nebular/auth';
+import { AuthService } from '../core/auth.service';
 
 @Component({
   selector: 'ngx-logout',
   templateUrl: './logout.component.html',
   styleUrls: ['./logout.component.scss']
 })
-export class LogoutComponent implements OnInit {
+export class LogoutComponent extends NbLogoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    protected router: Router,
+    ) {
+      super(authService, {}, router);
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit() {
+      if (
+        this.authService.isAdmin() ||
+        this.authService.isPrivileged ||
+        !this.authService.isInPublicMode) {
+          this.router.navigate(['hub', 'home']);
+        }
+      }
 
-}
+    }
