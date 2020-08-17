93f466ac6cfcc6ad0653b830ac2c56c5dfbcbd60
Risav Karna <risavkarna@gmail.com>
2020-08-17 09:44:39 +0545

    (minor, refactor) load service in child mod scope

--- hi/dashboard/src/app/services/dashboard/dashboard.service.ts
+++ hi/dashboard/src/app/services/dashboard/dashboard.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
+import { DashboardModule } from './dashboard.module';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: DashboardModule
 })
 export class DashboardService {
 
