de50e17b7ae4bf5a9601083fc0d709779ac54c5d
Risav Karna <risavkarna@gmail.com>
2020-08-18 12:51:47 +0545

    (fix) correct import and params

--- hi/dashboard/src/app/auth/reset-password/reset-pass.component.ts
+++ hi/dashboard/src/app/auth/reset-password/reset-pass.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NbResetPasswordComponent } from '@nebular/auth';
-import { BasicAuth } from 'app/models/auth-response.model';
+import { BasicAuth } from '../../models/auth-response.model';
 import { AuthService } from '../core/auth.service';
 
 @Component({
@@ -16,27 +16,27 @@ export class ResetPassComponent extends NbResetPasswordComponent implements OnIn
     protected router: Router,
     private route: ActivatedRoute,
     private changeDetector: ChangeDetectorRef
-    ) {
-      super(authService, {}, changeDetector, router);
-    }
+  ) {
+    super(authService, {}, changeDetector, router);
+  }
 
-    ngOnInit(): void {
-      if (
-        this.authService.isAdmin() ||
-        this.authService.isPrivileged ||
-        !this.authService.isInPublicMode) {
-          this.router.navigate(['hub', 'home']);
+  ngOnInit(): void {
+    if (
+      this.authService.isAdmin() ||
+      this.authService.isPrivileged ||
+      !this.authService.isInPublicMode) {
+      this.router.navigate(['hub', 'home']);
+    }
+  }
+
+  resetPass() {
+    const { email, password } = this.user;
+    this.authService.login(email, password)
+      .subscribe((val: BasicAuth.Response) => {
+        if (BasicAuth.isSuccess(val)) {
+          this.router.navigateByUrl('/hub/home');
         }
-      }
+      });
+  }
 
-      resetPass() {
-        const { email, password } = this.user;
-        this.authService.login(email, password, false)
-        .subscribe((val: BasicAuth.Response) => {
-          if (BasicAuth.isSuccess(val)) {
-            this.router.navigateByUrl('/hub/home');
-          }
-        });
-      }
-
-    }
+}
