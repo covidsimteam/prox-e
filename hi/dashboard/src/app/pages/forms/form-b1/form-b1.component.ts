191d967cd51f84b0f5f2e5088abf914b106ea499
Kripa Bhattarai <kripa@Kripas-MacBook-Air.local>
2020-08-18 18:13:36 +0545

    form updated

---
+++ hi/dashboard/src/app/pages/forms/form-b1/form-b1.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-form-pcr',
+  styleUrls: ['./form-b1.component.scss'],
+  templateUrl: './form-b1.component.html',
+})
+export class FormBComponent implements OnInit {
+
+  ngOnInit() {}
+
+}
