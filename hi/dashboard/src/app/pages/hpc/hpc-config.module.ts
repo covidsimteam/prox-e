d6049ca2171e7b5b63e646e5f75b3cb92a7ecb1d
Risav Karna <risavkarna@gmail.com>
2020-08-21 18:19:20 +0545

    (init) add new components for sys integration

---
+++ hi/dashboard/src/app/pages/hpc/hpc-config.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HpcConfigPanelComponent } from './hpc-config-panel/hpc-config-panel.component';
+
+
+
+@NgModule({
+  declarations: [HpcConfigPanelComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class HpcConfigModule { }
