bb57e5909ca7c055d15b11fdc99ea9a442ce392b
Risav Karna <risavkarna@gmail.com>
2020-08-21 18:23:25 +0545

    (init) add the last stubs needed for this sprint

---
+++ hi/dashboard/src/app/pages/facility/facility-assist.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FacilityAssistComponent } from './facility-assist/facility-assist.component';
+
+
+
+@NgModule({
+  declarations: [FacilityAssistComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class FacilityAssistModule { }
