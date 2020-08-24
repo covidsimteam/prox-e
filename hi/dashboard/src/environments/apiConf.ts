5ef8d46aa8e051aa153c23fdc778bb8bccdf9d5b
Risav Karna <risavkarna@gmail.com>
2020-08-24 09:02:55 +0545

    (fix) update db url

--- hi/dashboard/src/environments/apiConf.ts
+++ hi/dashboard/src/environments/apiConf.ts
@@ -2,6 +2,6 @@ export const appUris = {
   docDBUser: 'lalitpurlabadmin@nep.work',
   docDBPassword: 'public',
   devDocDB: 'http://localhost:5984/',
-  prodDocDB: 'https://covidb.cosys.work/db/',
+  prodDocDB: 'https://covidb.cosys.work/',
   stagingDocDB: 'http://covsimteam.cosys.work:5984/',
 };
