9089ac7745fa88192f3489978aa207182a9d2944
Risav Karna <risavkarna@gmail.com>
2020-08-27 02:36:31 +0545

    (update) add user profile & contact, fix imports

--- hi/cov-hub/src/app/pages/sample/sample-collection.module.ts
+++ hi/cov-hub/src/app/pages/sample/sample-collection.module.ts
@@ -1,16 +1,26 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { AllReturneesComponent } from './all-returnees/all-returnees.component';
+import { CloseAndCasualNetComponent } from './close-and-casual-net/close-and-casual-net.component';
 import { SampleCollectionComponent } from './sample-collection/sample-collection.component';
 import { SampleReportingComponent } from './sample-reporting/sample-reporting.component';
-import { CloseAndCasualNetComponent } from './close-and-casual-net/close-and-casual-net.component';
-import { AllReturneesComponent } from './all-returnees/all-returnees.component';
 
-
+const COMPS = [
+  SampleCollectionComponent,
+  SampleReportingComponent,
+  CloseAndCasualNetComponent,
+  AllReturneesComponent
+];
 
 @NgModule({
-  declarations: [SampleCollectionComponent, SampleReportingComponent, CloseAndCasualNetComponent, AllReturneesComponent],
+  declarations: [
+    ...COMPS
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    ...COMPS
   ]
 })
 export class SampleCollectionModule { }
