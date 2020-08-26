92ea07e9af6b621b88c31196340512b9d0b6952b
Risav Karna <risavkarna@gmail.com>
2020-08-26 09:40:53 +0545

    (update) add typings for translations

--- hi/cov-hub/src/app/app.module.ts
+++ hi/cov-hub/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -14,23 +14,28 @@ import {
   NbWindowModule
 } from '@nebular/theme';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { NgxEchartsModule } from 'ngx-echarts';
+import { from } from 'rxjs';
 import { AppConf, appConf, environment } from '../environments/environment';
 import { AuthInterceptor } from './@auth/interceptor/auth.interceptor';
 import { ErrorInterceptor } from './@auth/interceptor/error.interceptor';
+import { DashboardModule } from './@comp/dashboard/dashboard.module';
 import { CoreModule } from './@core/core.module';
 import { ThemeModule } from './@theme/theme.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { authSetup, formSetup } from './app.conf';
-import { DashboardModule } from './@comp/dashboard/dashboard.module';
 import { SelectionPipe } from './services/state/selection.pipe';
 import { SelectionsDirective } from './services/state/selections.directive';
 
 
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
+export class WebpackTranslateLoader implements TranslateLoader {
+  getTranslation(lang: 'en' | 'np') {
+    return from(
+      import(
+        `../assets/i18n/${lang}.json`
+      ));
+  }
 }
 @NgModule({
   declarations: [AppComponent, SelectionPipe, SelectionsDirective],
@@ -51,8 +56,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     TranslateModule.forRoot({
       loader: {
           provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
-          deps: [HttpClient]
+          useClass: WebpackTranslateLoader,
       }
     }),
     NgxEchartsModule.forRoot({
