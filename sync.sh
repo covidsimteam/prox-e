ea20d2a2c4937a5a3890c83d0a183e412256dd24
Risav Karna <risavkarna@gmail.com>
2020-08-26 14:45:09 +0545

    (chore) test sync runs config

--- sync.sh
+++ sync.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 type parallel >/dev/null 2>&1 || { echo >&2 "I require foo but it's not installed.  Aborting."; exit 1; }
+
+git merge master
+git merge develop
+
+git checkout develop
+
 cd ./sh
 bash ./metab.sh | parallel echo :::: - def-file
