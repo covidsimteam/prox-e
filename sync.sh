2ba13d288f37c8ffe8b05d66e5d244bfb5e09872
Risav Karna <risavkarna@gmail.com>
2020-08-26 13:54:54 +0545

    (chore) add git sync module for devs

---
+++ sync.sh
@@ -0,0 +1,14 @@
+git push
+
+git checkout master
+git merge develop 
+
+git checkout feature/scheduler
+git merge develop
+
+git checkout feature/wizard
+git merge develop
+
+git push
+git push
+git push 
