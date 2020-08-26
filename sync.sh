28fd99fc3c3340bb951eb9b28b9e714f7a0150fa
Risav Karna <risavkarna@gmail.com>
2020-08-26 14:41:29 +0545

    (chore) fix parallelism

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 type parallel >/dev/null 2>&1 || { echo >&2 "I require foo but it's not installed.  Aborting."; exit 1; }
-
-parallel -j 4 ./sh/metab.sh > response.log
+cd ./sh
+bash ./metab.sh | parallel echo :::: - def-file
