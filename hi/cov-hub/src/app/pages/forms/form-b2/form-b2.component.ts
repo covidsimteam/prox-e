7d49fce1f951e2b7a21d2c321dd78d96633cfe3e
Kripa Bhattarai <kripa.bhattarai22@gmail.com>
2020-09-02 11:09:52 +0545

    forms translation progress

--- hi/cov-hub/src/app/pages/forms/form-b2/form-b2.component.ts
+++ hi/cov-hub/src/app/pages/forms/form-b2/form-b2.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
+import { TranslationServiceEn } from '../../../services/i18n/translation-gen.service';
 
 @Component({
   selector: 'ngx-form-pcr',
@@ -6,6 +9,19 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './form-b2.component.html',
 })
 export class FormB2Component implements OnInit {
+
+  constructor(
+    private fb: FormBuilder,
+    private translator: TranslateService,
+    public t: TranslationServiceEn
+    ) {
+    translator.use('en');
+    // translator.get(t.fab.thahaChhaina)
+    //   .pipe(take(1))
+    //   .subscribe((chhaina: string) => {
+    //     this.caseStatus = chhaina;
+    //   });
+  }
 
   ngOnInit() {}
 
