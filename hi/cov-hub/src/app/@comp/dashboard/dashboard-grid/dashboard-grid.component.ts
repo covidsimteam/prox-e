5c9c1001931557ccad91c4d26a50dbcd18cfdf36
Risav Karna <1485929+risavkarna@users.noreply.github.com>
2020-09-03 20:07:55 +0545

    (update) separate the sero-prevalence charts

--- hi/cov-hub/src/app/@comp/dashboard/dashboard-grid/dashboard-grid.component.ts
+++ hi/cov-hub/src/app/@comp/dashboard/dashboard-grid/dashboard-grid.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MapSeroService } from '../map-sero.service';
 
 @Component({
   selector: 'cov-dashboard-grid',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardGridComponent implements OnInit {
 
-  constructor() { }
+  constructor(public mapSero: MapSeroService) { }
 
   ngOnInit(): void {
   }
