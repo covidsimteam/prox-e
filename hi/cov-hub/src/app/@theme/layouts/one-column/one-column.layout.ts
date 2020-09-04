ad8c96de234ca2946d54e64feb6b6546faeeac39
Saujan Ghimire <supertrampsookah@gmail.com>
2020-09-04 16:25:31 +0000

    feat(sidebar): add sidebar footer

--- hi/cov-hub/src/app/@theme/layouts/one-column/one-column.layout.ts
+++ hi/cov-hub/src/app/@theme/layouts/one-column/one-column.layout.ts
@@ -11,14 +11,14 @@ import { Component } from '@angular/core';
 
       <nb-sidebar class="menu-sidebar" tag="menu-sidebar" responsive>
         <ng-content select="nb-menu"></ng-content>
-      </nb-sidebar>
+        </nb-sidebar>
 
-      <nb-layout-column>
+        <nb-layout-column>
         <ng-content select="router-outlet"></ng-content>
-      </nb-layout-column>
+        </nb-layout-column>
 
-      <nb-layout-footer fixed>
-        <ngx-footer></ngx-footer>
+        <nb-layout-footer fixed>
+         <ngx-footer></ngx-footer>
       </nb-layout-footer>
     </nb-layout>
   `,
