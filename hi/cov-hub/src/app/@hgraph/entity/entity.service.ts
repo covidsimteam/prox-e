6127a3cf5a68cdcd65467dba3faaea953ca01398
Risav Karna <risavkarna@gmail.com>
2020-09-07 09:33:39 +0545

    (update) make entity service reactive

--- hi/cov-hub/src/app/@hgraph/entity/entity.service.ts
+++ hi/cov-hub/src/app/@hgraph/entity/entity.service.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
-export type ValueStream = Observable<any | unknown>;
+export type ValueStream = Observable<unknown>;
 @Injectable({
   providedIn: 'root'
 })
-export class EntityService {
+export abstract class EntityService {
 
   private values: ValueStream;
+
+  private valueSub = new BehaviorSubject<unknown>(null);
+  valueUpdates$ = this.valueSub.asObservable();
 
   constructor() { }
 
   get val(): ValueStream { return this.values; }
-  set val(headers: ValueStream) { this.values = headers; }
+
+  set val(headers: ValueStream) {
+    this.values = headers;
+    this.valueSub.next(headers);
+  }
+
 }
