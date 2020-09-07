3cf49b08cbe0b480a0c1f95be0b5e7ba6fc669e6
Risav Karna <risavkarna@gmail.com>
2020-09-07 20:24:38 +0545

    (merge) add dockerization

--- hi/cov-hub/docker/planet/nginx/time.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "HTTP/1.0 200 OK"
-echo "Content-type: text/plain"
-echo ""
-date +%s000
--- sh/b1.sh
+++ sh/b1.sh
@@ -2,15 +2,3 @@
 
 git checkout feature/scheduler
 git merge develop
-
-git checkout feature/wizard
-git merge develop
-
-git checkout feature/i18n
-git merge develop
-
-git checkout feature/forms
-git merge develop
-
-git checkout feat/global
-git merge develop
