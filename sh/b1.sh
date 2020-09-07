9c4ecab836586eb18ddbe4a0c7452fe4da189553
Risav Karna <risavkarna@gmail.com>
2020-09-07 10:27:12 +0545

    (chore) optimize sync routine

--- sh/b1.sh
+++ sh/b1.sh
@@ -2,3 +2,12 @@
 
 git checkout feature/scheduler
 git merge develop
+
+git checkout feature/wizard
+git merge develop
+
+git checkout feature/i18n
+git merge develop
+
+git checkout feature/forms
+git merge develop
--- sh/metab.sh
+++ sh/metab.sh
@@ -5,6 +5,5 @@ git fetch
 
 bash ./b0.sh
 bash ./b1.sh
-bash ./b2.sh
-bash ./b3.sh
+
 bash ./sync.sh
