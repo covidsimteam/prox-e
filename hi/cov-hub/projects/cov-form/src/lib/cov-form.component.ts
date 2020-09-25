bcbb453e4acd9a7008bcd039d5d256d9afbb39b0
Risav Karna <risavkarna@gmail.com>
2020-09-25 13:08:54 +0545

    (update) add form component lego bricks

---
+++ hi/cov-hub/projects/cov-form/src/lib/cov-form.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-cov-form',
+  template: `
+    <p>
+      cov-form works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class CovFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ hi/cov-hub/projects/cov-form/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of cov-form
+ */
+
+export * from './lib/cov-form.service';
+export * from './lib/cov-form.component';
+export * from './lib/cov-form.module';
