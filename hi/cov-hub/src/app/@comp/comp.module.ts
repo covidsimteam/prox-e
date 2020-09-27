8310a523f958847fb31147e0b77a2aa14c5a7f16
Risav Karna <risavkarna@gmail.com>
2020-09-27 10:37:50 +0545

    (refactor) remove unused imports

--- hi/cov-hub/src/app/@comp/comp.module.ts
+++ hi/cov-hub/src/app/@comp/comp.module.ts
@@ -1,8 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { CompComponent } from './comp/comp.component';
-import { CompDirective } from './comp/comp.directive';
-import { CompPipe } from './comp/comp.pipe';
+
 import { SchedComponent } from './sched/sched.component';
 import { SchedDirective } from './sched/sched.directive';
 import { SchedPipe } from './sched/sched.pipe';
@@ -17,9 +15,6 @@ import { CovTableMatModule } from './cov-table/mat/cov-table-mat.module';
 import { CovTableNebModule } from './cov-table/neb/cov-table-neb.module';
 
 const COMPS = [
-  CompComponent,
-  CompPipe,
-  CompDirective,
   SchedComponent,
   SchedPipe,
   SchedDirective,
