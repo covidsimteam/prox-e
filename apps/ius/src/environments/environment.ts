6b733c9981ac08306fbfdc0ba58c57f622686c9e
Risav Karna <risavkarna@gmail.com>
2020-09-28 08:33:04 +0545

    (update) add environment services

--- apps/ius/src/environments/environment.ts
+++ apps/ius/src/environments/environment.ts
@@ -1,16 +1,22 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+import { appUris } from './apiConf';
+import { UriConf, DBConf } from './env.types';
 
 export const environment = {
   production: false,
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+export class AppConf {
+
+  public uri: UriConf;
+  public db: DBConf; // TODO add restricted authorization for role public before using this on prod
+
+  constructor() {
+    this.uri = {
+      docDBUri: `${appUris.devDocDB}`,
+      authUri: `${appUris.devDocDB}_session`,
+    };
+    this.db = { docDBUser: appUris.docDBUser, docDBPassword: appUris.docDBPassword };
+  }
+}
+
+export const appConf = new AppConf();
